67d6c0504d78e8583f42ee788d6810d1f178ac98
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-11 00:22:11 +0530

    Added @CrossOrigin in controller

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/controller/ToolDetailsController.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/controller/ToolDetailsController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/ToolDetails")
+@CrossOrigin
 public class ToolDetailsController {
     @Autowired
     private  ToolDetailsService toolDetailsService;
