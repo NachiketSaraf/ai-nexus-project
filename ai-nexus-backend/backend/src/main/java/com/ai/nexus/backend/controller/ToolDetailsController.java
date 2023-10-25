2614a65c6ab804ac8579c381eeddbf696d8aea88
NachiketSaraf <sarafnachiket@gmail.com>
2023-10-25 19:58:23 +0530

    ...

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/controller/ToolDetailsController.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/controller/ToolDetailsController.java
@@ -21,7 +21,7 @@ public class ToolDetailsController {
     @Autowired
     private ToolDetailsService toolDetailsService;
 
-
+    // Get list of all details
     @GetMapping("/all")
     public List<ToolDetails> getAllToolDetails() {
         return toolDetailsService.getAllToolDetails();
