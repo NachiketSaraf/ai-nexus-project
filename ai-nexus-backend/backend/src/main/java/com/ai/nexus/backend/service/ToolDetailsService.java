8128aafdcf846060e1e1162c5f6a5661ee0f2f56
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-03 04:07:16 +0530

    discarded tool card direct using tool detail for tool card insert and patch api

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/service/ToolDetailsService.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/service/ToolDetailsService.java
@@ -19,53 +19,6 @@ public class ToolDetailsService {
     @Autowired
     private  ToolDetailsRepository toolDetailsRepository;
 
-<<<<<<< HEAD
-=======
-    @Autowired
-    private CategoryRepository categoryRepository;
-    @Transactional
-    public List<ToolDetails> createToolDetails(List<ToolDetails> toolDetailsList) {
-        List<ToolDetails> savedToolDetails = new ArrayList<>();
-
-        for (ToolDetails toolDetails : toolDetailsList) {
-            String categoryName = toolDetails.getCategory().getCategoryName();
-
-            // Check if the category with the specified name exists in the database
-            Category category = categoryRepository.findByCategoryName(categoryName);
-
-            // If the category doesn't exist, create a new one
-            if (category == null) {
-                category = new Category();
-                category.setCategoryName(categoryName);
-                categoryRepository.save(category);
-            }
-
-            toolDetails.setCategory(category);
-            savedToolDetails.add(toolDetailsRepository.save(toolDetails));
-        }
-
-        return savedToolDetails;
-    }
-    public List<ToolDetails> updateToolImages(List<Map<String, String>> toolUpdates) {
-        List<ToolDetails> updatedTools = new ArrayList<>();
-
-        for (Map<String, String> toolUpdate : toolUpdates) {
-            String toolName = toolUpdate.get("toolName");
-            String toolImage = toolUpdate.get("toolImage");
-
-            if (toolName != null && toolImage != null) {
-                ToolDetails tool = toolDetailsRepository.findByToolName(toolName);
-                if (tool != null) {
-                    tool.setToolImage(toolImage);
-                    updatedTools.add(toolDetailsRepository.save(tool));
-                }
-            }
-        }
-
-        return updatedTools;
-    }
-
->>>>>>> c9b064d (discarded tool card)
 
     public List<ToolDetails> getAllToolDetails() {
         return toolDetailsRepository.findAll();
@@ -80,50 +33,7 @@ public class ToolDetailsService {
     public Optional<ToolDetails> getToolDetailById(Long id) {
         return toolDetailsRepository.findById(id);
     }
-    public ToolDetails updateToolDetails(Long toolId, ToolDetails updatedToolDetails) {
-        ToolDetails existingTool = toolDetailsRepository.findById(toolId)
-                .orElseThrow(() -> new ResourceNotFoundException("Tool not found with id: " + toolId));
 
-        // Update all fields
-        existingTool.setToolName(updatedToolDetails.getToolName());
-        existingTool.setToolImage(updatedToolDetails.getToolImage());
-        existingTool.setToolFeatures(updatedToolDetails.getToolFeatures());
-        existingTool.setToolWebsiteLink(updatedToolDetails.getToolWebsiteLink());
-        existingTool.setToolDemoVideoLinks(updatedToolDetails.getToolDemoVideoLinks());
-        existingTool.setCategory(updatedToolDetails.getCategory());
-        existingTool.setToolDescription(updatedToolDetails.getToolDescription());
-
-        return toolDetailsRepository.save(existingTool);
-    }
-
-    public List<ToolDetails> getToolByTag(String toolTag) {
-        List<ToolDetails> existingTool =  toolDetailsRepository.findByToolTag(toolTag);
-//                .orElseThrow(() -> new ResourceNotFoundException("Tool not found with tag: " + tag));
-
-        if (toolTag.equals("trending")){
-            return toolSelector(existingTool,3); // for trending
-        }else {
-            return toolSelector(existingTool,6); // for recommend
-        }
-    }
-
-    private List<ToolDetails> toolSelector(List<ToolDetails> existingTool, int numberOfCard) {
-        Set<ToolDetails> selectedTool = new HashSet<>();
-        Random random = new Random();
-        while (selectedTool.size() < numberOfCard){
-            selectedTool.add(existingTool.get(random.nextInt(existingTool.size())));
-        }
-        List<ToolDetails> ans = selectedTool.stream().toList();
-        return  ans;
-    }
-//
-//    public Category findCategoryByName(String categoryName) {
-//        return categoryRepository.findByCategoryName(categoryName);
-//    }
-//
-//    public ToolDetails saveToolDetails(ToolDetails toolDetails) {
-//        return toolDetailsRepository.save(toolDetails);
-//    }
 //
 //    public ToolDetails createToolDetail(ToolDetails toolDetail) {
 //        return toolDetailsRepository.save(toolDetail);
