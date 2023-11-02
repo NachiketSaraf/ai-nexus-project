4dbc48bdb2f23a936202c156c2101eff84baad1a
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-03 04:07:16 +0530

    discarded tool card direct using tool detail for tool card insert and patch api

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/service/ToolDetailsService.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/service/ToolDetailsService.java
@@ -1,8 +1,12 @@
 package com.ai.nexus.backend.service;
 
 
+import com.ai.nexus.backend.model.Category;
 import com.ai.nexus.backend.model.ToolDetails;
+import com.ai.nexus.backend.repository.CategoryRepository;
 import com.ai.nexus.backend.repository.ToolDetailsRepository;
+import jakarta.transaction.Transactional;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +18,33 @@ public class ToolDetailsService {
     @Autowired
     private  ToolDetailsRepository toolDetailsRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Transactional
+    public ToolDetails insertToolDetails(ToolDetails toolDetails) {
+        String categoryName = toolDetails.getCategory().getCategoryName();
+
+        // Check if the category with the specified name exists in the database
+        Category category = categoryRepository.findByCategoryName(categoryName);
+
+        // If the category doesn't exist, create a new one
+        if (category == null) {
+            category = new Category();
+            category.setCategoryName(categoryName);
+            categoryRepository.save(category);
+        }
+
+        toolDetails.setCategory(category);
+
+        return toolDetailsRepository.save(toolDetails);
+    }
+
 
     public List<ToolDetails> getAllToolDetails() {
         return toolDetailsRepository.findAll();
+    }
+    public List<ToolDetails> getToolDetailsByCategory(String categoryName) {
+        return toolDetailsRepository.findByCategory_CategoryName(categoryName);
     }
     public ToolDetails getToolDetailsByName(String name) {
         return toolDetailsRepository.findByToolName(name);
@@ -25,7 +53,29 @@ public class ToolDetailsService {
     public Optional<ToolDetails> getToolDetailById(Long id) {
         return toolDetailsRepository.findById(id);
     }
+    public ToolDetails updateToolDetails(Long toolId, ToolDetails updatedToolDetails) {
+        ToolDetails existingTool = toolDetailsRepository.findById(toolId)
+                .orElseThrow(() -> new ResourceNotFoundException("Tool not found with id: " + toolId));
 
+        // Update all fields
+        existingTool.setToolName(updatedToolDetails.getToolName());
+        existingTool.setToolImage(updatedToolDetails.getToolImage());
+        existingTool.setToolFeatures(updatedToolDetails.getToolFeatures());
+        existingTool.setToolWebsiteLink(updatedToolDetails.getToolWebsiteLink());
+        existingTool.setToolDemoVideoLinks(updatedToolDetails.getToolDemoVideoLinks());
+        existingTool.setCategory(updatedToolDetails.getCategory());
+        existingTool.setToolDescription(updatedToolDetails.getToolDescription());
+
+        return toolDetailsRepository.save(existingTool);
+    }
+//
+//    public Category findCategoryByName(String categoryName) {
+//        return categoryRepository.findByCategoryName(categoryName);
+//    }
+//
+//    public ToolDetails saveToolDetails(ToolDetails toolDetails) {
+//        return toolDetailsRepository.save(toolDetails);
+//    }
 //
 //    public ToolDetails createToolDetail(ToolDetails toolDetail) {
 //        return toolDetailsRepository.save(toolDetail);
