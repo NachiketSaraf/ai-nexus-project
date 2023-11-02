22ec15c8de327338197e84d87382a38d90e9aad5
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-03 04:07:08 +0530

    discarded tool card direct using tool detail for tool card insert and patch api

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/controller/ToolDetailsController.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/controller/ToolDetailsController.java
@@ -8,11 +8,6 @@ import com.ai.nexus.backend.service.ToolDetailsService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-=======
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.http.ResponseEntity;
->>>>>>> 130f04f (discarded tool card)
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -41,10 +36,14 @@ public class ToolDetailsController {
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     @GetMapping("by-id/{id}")
 =======
     @GetMapping("test/{id}")
 >>>>>>> 45cbfe9 (discarded tool card)
+=======
+    @GetMapping("by-id/{id}")
+>>>>>>> 0df5dc5 (discarded tool card)
     public ToolDetails getToolDetailsById(@PathVariable Long id) {
         Optional<ToolDetails> toolDetails = toolDetailsService.getToolDetailById(id);
         if (toolDetails.isPresent()) {
@@ -85,6 +84,24 @@ public class ToolDetailsController {
 =======
 =======
 >>>>>>> 138c711 (updated to get all card)
+    @GetMapping("/byCategory/{categoryName}")
+    public ResponseEntity<List<ToolDetails>> getToolDetailsByCategory(@PathVariable String categoryName) {
+        List<ToolDetails> toolDetails = toolDetailsService.getToolDetailsByCategory(categoryName);
+        return ResponseEntity.ok(toolDetails);
+    }
+    @PostMapping("/insert")
+    public ToolDetails insertToolDetails(@RequestBody ToolDetails toolDetails) {
+        return toolDetailsService.insertToolDetails(toolDetails);
+    }
+
+    @PatchMapping("/{toolId}")
+    public ResponseEntity<ToolDetails> updateToolDetails(@PathVariable Long toolId, @RequestBody ToolDetails updatedToolDetails) {
+        ToolDetails updatedTool = toolDetailsService.updateToolDetails(toolId, updatedToolDetails);
+        return ResponseEntity.ok(updatedTool);
+    }
+
+}
+
     @GetMapping("/byCategory/{categoryName}")
     public ResponseEntity<List<ToolDetails>> getToolDetailsByCategory(@PathVariable String categoryName) {
         List<ToolDetails> toolDetails = toolDetailsService.getToolDetailsByCategory(categoryName);
