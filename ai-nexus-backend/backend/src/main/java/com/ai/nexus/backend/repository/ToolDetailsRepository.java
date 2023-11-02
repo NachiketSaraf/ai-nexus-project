7924521603e9881b8e2d5cd49519eb6a46d6a86c
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-03 04:07:01 +0530

    discarded tool card direct using tool detail for tool card insert and patch api

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/repository/ToolDetailsRepository.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/repository/ToolDetailsRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ToolDetailsRepository extends JpaRepository<ToolDetails, Long> {
     ToolDetails findByToolName(String toolName);
     // Define custom query methods if needed
+    List<ToolDetails> findByCategory_CategoryName(String categoryName);
+
 }
