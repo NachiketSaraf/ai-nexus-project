c9f021aacdb7b36df90d485203e6476a5b139d4b
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-03 04:06:45 +0530

    discarded tool card direct using tool detail for tool card insert and patch api

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/service/ToolCardService.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/service/ToolCardService.java
@@ -1,11 +1,22 @@
 package com.ai.nexus.backend.service;
 
+
 import com.ai.nexus.backend.model.Category;
 import com.ai.nexus.backend.model.ToolCard;
 import com.ai.nexus.backend.repository.ToolCardRepository;
+import com.ai.nexus.backend.utils.SimpleCardUtils;
+
+import com.ai.nexus.backend.utils.CategoryButtonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class ToolCardService {
