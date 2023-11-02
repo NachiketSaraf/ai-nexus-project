8cb6746bd01fc5bbd92e288c91d0e722690b7cb6
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-03 04:06:54 +0530

    discarded tool card direct using tool detail for tool card insert and patch api

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/model/ToolDetails.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/model/ToolDetails.java
@@ -34,10 +34,14 @@ public class ToolDetails {
     private String toolDemoVideoLinks;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b243707 (discarded tool card)
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 =======
@@ -45,6 +49,8 @@ public class ToolDetails {
     private String toolTag;
 >>>>>>> e389f31 (New api to get tools with trending and recommended)
 
+=======
+>>>>>>> b243707 (discarded tool card)
     @Column(name = "tool_description", columnDefinition = "text")
     private String toolDescription;
 }
@@ -82,8 +88,11 @@ public class ToolDetails {
 //    @Column(name = "tool_demo_video_links", columnDefinition = "text")
 //    private String toolDemoVideoLinks;
 
+<<<<<<< HEAD
 =======
 >>>>>>> 07de33c (discarded tool card)
+=======
+>>>>>>> b243707 (discarded tool card)
 //    @OneToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "tool_card_id")
 //    private ToolCard toolCard;
