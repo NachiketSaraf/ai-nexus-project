3e6e2ac57070c25409a07c9d4e14d6983f4d9735
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-11-04 10:06:19 +0530

    stuff

--- ai-nexus-UI/nexus-ui/src/app/app.module.ts
+++ ai-nexus-UI/nexus-ui/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
+import { ViewComponent } from './view/view.component';
 //import {MatIconModule} from '@angular/material/icon';
 
 
@@ -29,7 +30,8 @@ import { HomeComponent } from './home/home.component';
     NavbarComponent,
     CardComponent,
     AiToolsComponent,
-    HomeComponent
+    HomeComponent,
+    ViewComponent
 
   ],
   imports: [
--- ai-nexus-UI/nexus-ui/src/app/card/card.component.ts
+++ ai-nexus-UI/nexus-ui/src/app/card/card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CardService } from '../service/card.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -7,11 +8,43 @@ import { CardService } from '../service/card.service';
   styleUrls: ['./card.component.scss'],
   // standalone: true, 
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
   @Input() selectedCategory: string="";
-  card: any[] = [];
-  constructor(private cardService: CardService) { }
-  // ngOnInit(): void {
+  card: any[] = []; // Array to store card data
+
+  constructor(private cardService: CardService) {}
+
+  ngOnInit() {
+    this.fetchCard();
+  }
+
+  fetchCard() {
+    const selectedCategory = 'Image Generator';
+    this.cardService.getCard(selectedCategory).subscribe((card) => {
+      this.card = card;
+    });
+  }
+}
+  
+//previous code 1
+// @Input() selectedCategory: string="";
+// card: any[] = [];
+// constructor(private cardService: CardService , private route: ActivatedRoute) { }
+
+// ngOnInit(): void{
+//   this.route.params.subscribe((params) => {
+//     this.selectedCategory = params['selectedCategory'];
+//     if(this.selectedCategory){
+//       this.cardService.getCard(this.selectedCategory).subscribe((card) => {
+//         console.log(card);
+//         this.card =card ;
+//       });
+//     }
+//   });
+// }
+
+//previous code 
+// ngOnInit(): void {
   //   this.getCard()
   //   console.log(this.getCard());
   // }
@@ -21,13 +54,10 @@ export class CardComponent {
   //     console.log(this.card);
   //   })
   // }
-  ngOnChanges(): void {
-    // Fetch cards for the selected category from your backend service
-    if (this.selectedCategory) {
-      this.cardService.getCard(this.selectedCategory).subscribe((data: any[]) => {
-        this.card = data;
+  // ngOnChanges(): void {
+  //   // Fetch cards for the selected category from your backend service
+  //   if (this.selectedCategory) {
+  //     this.cardService.getCard(this.selectedCategory).subscribe((card) => {
+  //       this.card = card;
 
-      });
-    }
-  }
-}
+  //     });
