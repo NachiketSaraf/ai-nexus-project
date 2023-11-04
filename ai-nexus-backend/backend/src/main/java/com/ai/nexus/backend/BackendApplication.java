7e117bd2afe058495acd4d93b1ba80b2313d6d1a
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-04 18:39:13 +0530

    added swagger

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/BackendApplication.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/BackendApplication.java
@@ -1,6 +1,10 @@
 package com.ai.nexus.backend;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+<<<<<<< HEAD
+=======
+import io.swagger.v3.oas.annotations.info.Contact;
+>>>>>>> c00b80d (added swagger)
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
