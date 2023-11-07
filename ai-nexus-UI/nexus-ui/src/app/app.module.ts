92373659f7aedbac5ba4f2a4c4b7d27b4ed756da
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-11-07 23:25:14 +0530

    ..

--- ai-nexus-UI/nexus-ui/src/app/app.module.ts
+++ ai-nexus-UI/nexus-ui/src/app/app.module.ts
@@ -14,36 +14,17 @@ import { CardService } from './service/card.service';
 import { AiToolsComponent } from './ai-tools/ai-tools.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-<<<<<<< HEAD
+
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { TrendingComponent } from './trending/trending.component';
-import { RecommendedComponent } from './recommended/recommended.component';
-=======
-import { ViewComponent } from './view/view.component';
->>>>>>> 32be760 (stuff)
-=======
+
 
 import { TrendingComponent } from './trending/trending.component';
 import { RecommendedComponent } from './recommended/recommended.component';
->>>>>>> d5a9c5c (category)
-=======
->>>>>>> 39281af7f2aad36404aeb5eb48de6a6c10bdefbb
-//import {MatIconModule} from '@angular/material/icon';
 
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 39281af7f2aad36404aeb5eb48de6a6c10bdefbb
-import { HomeComponent } from './home/home.component';
-import { RecommendedComponent } from './recommended/recommended.component';
-import { TrendingComponent } from './trending/trending.component';
 
->>>>>>> 39281af (..)
+
 
 @NgModule({
   declarations: [
@@ -53,40 +34,27 @@ import { TrendingComponent } from './trending/trending.component';
     CardComponent,
     AiToolsComponent,
     HomeComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     TrendingComponent,
     RecommendedComponent,
+
     
-=======
-    ViewComponent
->>>>>>> 32be760 (stuff)
-=======
-    TrendingComponent,
-    RecommendedComponent,
-    
->>>>>>> d5a9c5c (category)
+
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
     HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
     // MatIconModule
-<<<<<<< HEAD
-
     BrowserAnimationsModule
 
-=======
-    BrowserAnimationsModule
->>>>>>> 39281af7f2aad36404aeb5eb48de6a6c10bdefbb
-  ],
-  providers: [CategoryService, CardService],
+  ], 
+   providers: [CategoryService, CardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
