9ea63d0f61b01d9abd32d4a5a9355b40197e96da
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-07 15:51:10 +0530

    card

--- ai-nexus-UI/nexus-ui/src/app/service/card.service.ts
+++ ai-nexus-UI/nexus-ui/src/app/service/card.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
-  
+  cards: any[] = [];
   private baseUrl = 'http://localhost:8080'; // Replace with your backend URL
   constructor(private http: HttpClient) {} 
   
-    getCard(categoryName :string ): Observable<string[]>{
-      const url = `${this.baseUrl}/api/tool-details${categoryName}`;
-      return this.http.get<string[]>(url) ;
+    getCard(categoryName :string ): Observable<any>{
+      const url = `${this.baseUrl}/api/tool-details/${categoryName}`;
+      console.log(url);
+      return this.http.get(url);
+    }
+    setCards(cards: any[]) {
+      this.cards = cards;
+    }
+  
+    getCards(): any[] {
+      return this.cards;
     }
     
 }
