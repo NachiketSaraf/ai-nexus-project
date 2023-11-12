a39cb9c287880eed52ef167e9e201b12688ecc50
NachiketSaraf <sarafnachiket@gmail.com>
2023-11-12 09:53:00 +0530

    ...

--- ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/model/ToolDetails.java
+++ ai-nexus-backend/backend/src/main/java/com/ai/nexus/backend/model/ToolDetails.java
@@ -1,6 +1,5 @@
 package com.ai.nexus.backend.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
