686430986c3adb8f08e02525d3dd441b504103f5
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2024-02-22 00:01:36 +0530

    changes

--- ai-nexus-UI/nexus-ui/src/app/navbar/navbar.component.ts
+++ ai-nexus-UI/nexus-ui/src/app/navbar/navbar.component.ts
@@ -6,27 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-openSearch(){}
+// openSearch(){}
 selectedButton: string = ''; // Variable to keep track of the selected button
 
   // Method to update the selected button
-  selectButton(button: string): void {
-    this.selectedButton = button;
-    this.hideDropdown();
-  }
+  
 
   isDropdownVisible: boolean = false;
-
-  toggleDropdown(): void {
-      this.isDropdownVisible = !this.isDropdownVisible;
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
   }
 
-  hideDropdown(): void {
-      this.isDropdownVisible = false;
+  selectButton(button: string) {
+    this.selectedButton = button;
   }
-  // openSearch(name : string): any{
-  //   this.openSearch = name;
-  // }
 
-  
+  hideDropdown() {
+    this.isDropdownVisible = false;
+  }
 }
