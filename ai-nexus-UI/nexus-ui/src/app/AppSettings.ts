ecaeea188200d564d5485a5a3cbf3970f1377f9d
NachiketSaraf <sarafnachiket@gmail.com>
2024-03-18 23:50:32 +0530

    ...

--- ai-nexus-UI/nexus-ui/src/app/AppSettings.ts
+++ ai-nexus-UI/nexus-ui/src/app/AppSettings.ts
@@ -1,6 +1,7 @@
 export class AppSettings{
     // public static BACKEND_API = 'http://20.231.1.192:8080'; //azure
     public static BACKEND_API = 'http://3.87.139.191:8080'; //aws
+    // public static BACKEND_API = 'http://3.87.139.19:8080'; //aws
 
     
 }
